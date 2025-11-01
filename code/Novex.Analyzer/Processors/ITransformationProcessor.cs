f5dd42432fe93cbd1b402977fc5f28f3d2416870
Wizcas Chen <chen@0x1c.dev>
2025-11-02 02:30:27 +0800

    delete rule engine v1

--- code/Novex.Analyzer/Processors/ITransformationProcessor.cs
+++
@@ -1,9 +0,0 @@
-namespace Novex.Analyzer.Processors;
-
-/// <summary>
-/// 转换处理器接口
-/// </summary>
-public interface ITransformationProcessor
-{
-  Task<string> ProcessAsync(string input, Dictionary<string, object> parameters);
-}
